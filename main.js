871fa671fe7cec79ec891464a0bb4a4c7de426c2
arjomand-dev <ma.arjomand1387@gmail.com>
2024-08-31 23:17:16 +0330

    v0.0.1

---
+++ main.js
@@ -0,0 +1,233 @@
+import Juniora, {image, size, sound} from './core/juniora-mg.js'
+
+const game = new Juniora
+
+game.characters(
+    {
+        pouria: {
+            avatar: ('http://127.0.0.1:5500/images/pouria.jpg'),
+            size: 'xx-lg',
+            health: 6000
+        },
+        gun: {
+            avatar: ('http://127.0.0.1:5500/images/gun.png'),
+            size: 'md',
+            direction: 'right',
+        },
+        ammo: {
+            avatar: ('http://127.0.0.1:5500/images/ammo.png'),
+            size: 'x-sm',
+            flip: true
+        },
+        gun2: {
+            avatar: ('http://127.0.0.1:5500/images/gun2.png'),
+            size: 'xx-lg',
+            direction: 'right',
+        },
+        ammo2: {
+            avatar: ('http://127.0.0.1:5500/images/ammo2.png'),
+            size: 'x-sm'
+        },
+        gun3: {
+            avatar: ('http://127.0.0.1:5500/images/gun3.png'),
+            size: 'xx-lg',
+            direction: 'right',
+        },
+        ammo3: {
+            avatar: ('http://127.0.0.1:5500/images/ammo3.png'),
+            size: 'x-sm'
+        },
+        netaniaho: {
+            avatar: ('http://127.0.0.1:5500/images/neta.jpg'),
+            size: 'md',
+            health: 600,   
+        },
+        Trump: {
+            avatar: 'http://127.0.0.1:5500/images/trump.jpg',
+            size: 'x-lg',
+            health: 800
+        }
+    }
+)
+
+game.setup(
+    e => {
+            localStorage.kills = 0
+            localStorage.record = localStorage.record === undefined ? 0 : localStorage.record
+            document.getElementById('kills').innerText = localStorage.kills
+            document.getElementById('record').innerText = localStorage.record
+          
+            // POURIA
+            const pouria = e.spawnCharacter('pouria')
+            pouria.activateUserControlls()
+            pouria.activatePhysics()
+            const pouriaGun = pouria.take('gun')
+            let pouriaGun2, pouriaGun3
+            let pouriaAmmo = ''
+            pouria.moveRight(400)
+            pouria.onHit((name) => {
+                let damage = name === 'ammo' ? 20 : 50
+                pouria.health(pouria.health()-damage, true)
+                return true
+            })
+            pouria.onDied(() => {
+                setTimeout(() => {
+                    localStorage.record = localStorage.kills > localStorage.record ? localStorage.kills : localStorage.record
+                    alert('You lost!\nClick "OK" to start again')
+                    location.reload()
+                }, 1200)
+            })
+
+            let ammoCount = 7
+            const ammoMonitor = document.querySelector('.ammo-count')
+            function fillAmmo() {
+                ammoMonitor.innerHTML = ''
+                for (let i = 0; i <ammoCount; i++) {
+                    let ammo = image('http://127.0.0.1:5500/images/ammo' + pouriaAmmo +'.png')
+                    ammo.style.transform = pouriaAmmo === '' ? 'rotate(90deg)' : 'rotate(270deg)'
+                    ammo.width = 20
+                    ammoMonitor.appendChild(ammo)
+                }
+            }
+            fillAmmo()
+
+            let allowFillAmmo = false
+            e.keyEvent(['Shift'], () => {
+                let spawnPoint
+                if (pouria.getDirection() === 'right') {
+                    spawnPoint = [pouria.getLocation()[0] + pouria.getSize()[0]*0.5,pouria.getLocation()[1] + pouria.getSize()[1]]
+                }
+                else {
+                    spawnPoint = [pouria.getLocation()[0] + pouria.getSize()[0]*0.5, pouria.getLocation()[1]-30]
+                }
+                const direction = pouria.getDirection()
+                const speed = 50
+                if (ammoCount > 0) {
+                    e.shoot('ammo' + pouriaAmmo, spawnPoint, direction, speed)
+                    ammoCount--
+                    ammoMonitor.removeChild(ammoMonitor.lastChild)
+                    sound('http://127.0.0.1:5500/audios/bang.mp3')
+                }
+            })
+            e.keyEvent('Control', () => {
+                if (allowFillAmmo) {
+                    fillAmmo()
+                    sound('http://127.0.0.1:5500/audios/change.mp3')
+                    allowFillAmmo = false
+                }
+                ammoCount = pouriaAmmo === '' ? 7 : (pouriaAmmo === '2' ? 12 : 50)
+            })
+
+            setInterval(() => {
+                allowFillAmmo = true
+            }, 3000)
+
+
+            // NETANIAHO
+            function spawnNetaniaho() {
+                const netaniaho = e.spawnCharacter('netaniaho')
+                netaniaho.activatePhysics()
+                let max = document.body.clientWidth - +(size('xx-lg'))
+                let random = Math.floor(Math.random() * (max+1))
+                netaniaho.moveRight(random)
+                netaniaho.take('gun')
+                netaniaho.moveLeft(1)
+                netaniaho.onHit((name) => {
+                    let damage = name === 'ammo' ? 20 : 50
+                    netaniaho.health(netaniaho.health()-damage, true)
+                    return true
+                })
+                netaniaho.onDied(() => {
+                    if (localStorage.kills === '20') {
+                        spawnTrump()
+                    }
+                    else {
+                        spawnNetaniaho()
+                    }
+                    localStorage.kills = +(localStorage.kills)+1
+                    document.getElementById('kills').innerText = localStorage.kills
+                    if (localStorage.kills === '5') {
+                        pouria.untake(pouriaGun)
+                        pouriaGun2 = pouria.take('gun2')
+                        pouriaAmmo = '2'
+                        allowFillAmmo = true
+                        ammoCount = 12
+                        fillAmmo()
+                    }
+                })
+
+                const netaniahoShoot = setInterval(() => {
+                    if (pouria.getLocation()[1] > netaniaho.getLocation()[1] && netaniaho.getDirection() === 'left') {
+                        netaniaho.moveRight(1)
+                    }
+                    else if (pouria.getLocation()[1] < netaniaho.getLocation()[1] && netaniaho.getDirection() === 'right') {
+                        netaniaho.moveLeft(1)
+                    }
+                    let spawnPoint
+                    if (netaniaho.getDirection() === 'right') {
+                        spawnPoint = [netaniaho.getLocation()[0] + netaniaho.getSize()[0]*0.5,netaniaho.getLocation()[1] + netaniaho.getSize()[1]]
+                    }
+                    else {
+                        spawnPoint = [netaniaho.getLocation()[0] + netaniaho.getSize()[0]*0.5, netaniaho.getLocation()[1]-30]
+                    }
+                    e.shoot('ammo', spawnPoint, netaniaho.getDirection(), 50)
+                    sound('http://127.0.0.1:5500/audios/bang.mp3')
+                    if (netaniaho.health() <= 0) {
+                        clearInterval(netaniahoShoot)
+                    }
+                }, 800)
+            }
+            spawnNetaniaho()
+
+            function spawnTrump() {
+                const Trump = e.spawnCharacter('Trump')
+                Trump.activatePhysics()
+                let max = document.body.clientWidth - +(size('xx-lg'))
+                let random = Math.floor(Math.random() * (max+1))
+                Trump.moveRight(random)
+                Trump.take('gun2')
+                Trump.moveLeft(1)
+                Trump.onHit((name) => {
+                    let damage = name === 'ammo2' ? 50 : 200
+                    Trump.health(Trump.health()-damage, true)
+                    return true
+                })
+                Trump.onDied(() => {
+                    spawnTrump()
+                    localStorage.kills = +(localStorage.kills)+1
+                    document.getElementById('kills').innerText = localStorage.kills
+                    if (localStorage.kills === '30') {
+                        pouria.untake(pouriaGun2)
+                        pouria.take('gun3')
+                        pouriaAmmo = '3'
+                        allowFillAmmo = true
+                        ammoCount = 50
+                        fillAmmo()
+                    }
+                })
+            
+                const TrumpShoot = setInterval(() => {
+                    if (pouria.getLocation()[1] > Trump.getLocation()[1] && Trump.getDirection() === 'left') {
+                        Trump.moveRight(1)
+                    }
+                    else if (pouria.getLocation()[1] < Trump.getLocation()[1] && Trump.getDirection() === 'right') {
+                        Trump.moveLeft(1)
+                    }
+                    let spawnPoint
+                    if (Trump.getDirection() === 'right') {
+                        spawnPoint = [Trump.getLocation()[0] + Trump.getSize()[0]*0.5,Trump.getLocation()[1] + Trump.getSize()[1]]
+                    }
+                    else {
+                        spawnPoint = [Trump.getLocation()[0] + Trump.getSize()[0]*0.5, Trump.getLocation()[1]-30]
+                    }
+                    e.shoot('ammo2', spawnPoint, Trump.getDirection(), 50)
+                    sound('http://127.0.0.1:5500/audios/bang.mp3')
+                    if (Trump.health() <= 0) {
+                        clearInterval(TrumpShoot)
+                    }
+                }, 800)
+            }
+        }
+)
+
+game.start()
